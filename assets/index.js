a6735cad92e9133d17991ef1de6a463f959ad75c
StaplezStudio <Whataworker@gmail.com>
2025-06-25 20:12:33 -0400

    Add missing assets

--- assets/index.js
+++ assets/index.js
@@ -1,2 +1,6 @@
-// ...add your JavaScript code here...
-console.log('index.js loaded');
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
